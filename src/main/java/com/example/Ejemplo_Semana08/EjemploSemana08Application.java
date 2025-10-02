a489ba4e2cb72756af4a8555c26ce59178148a4a
Carlos <camposangelescarlos@gmail.com>
2025-10-02 12:19:06 -0500

    Proyecto inicial en Spring Boot

---
+++ src/main/java/com/example/Ejemplo_Semana08/EjemploSemana08Application.java
@@ -0,0 +1,13 @@
+package com.example.Ejemplo_Semana08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSemana08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSemana08Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ejemplo_Semana08/EjemploSemana08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ejemplo_Semana08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSemana08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
